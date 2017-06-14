b724a8ec2d82b05c040239e4e09c3ae86ae31725
Keionne Derousselle <Keionne.Derousselle@extendhealth.com>
2017-06-14 17:22:24 -0600

    creating cells

---
+++ src/components/images/ImageCell.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class ImageCell extends React.Component{
+    constructor(){
+        super();
+        this.state = { 
+            imageUri: '', 
+            cellWidth: 0,
+            cellHeight: 0,
+            selected: false
+        }
+    }
+
+
+
+    render(){
+        return(
+            <div className={this.state.selected ? 'selected-img-cell' : 'img-cell'}>
+                {
+                    this.state.imageUri ?
+                    <image 
+                    src={this.state.imageUri}
+                    height={this.state.cellHeight}
+                    width={this.state.cellWidth}
+                    className='img-responsive' /> :
+                    null
+                }
+            </div>
+        );
+    }
+}
+
+export default ImageCell;
--- src/components/images/ImageGrid.js
+++ src/components/images/ImageGrid.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import ImageCell from './ImageCell';
 
 class ImageGrid extends React.Component{
     constructor(props){
         super(props);
+        const NUMBER_OF_CELLS = 6;
+        const NUMBER_OF_ROWS = 3;
+        this.state = { 
+            numberOfPictureCells : NUMBER_OF_CELLS,
+            numberOfPictureRows: 3,
+            cells: new Array(NUMBER_OF_CELLS)
+        };
     }
+
     render(){
         let image = <img className='img-reponsive' src={this.props.image} />
-        let content = this.props.image? image : <span>Load an Image</span>;
+        let rows = new Array(NUMBER_OF_ROWS);
+        let cellsPerRow = NUMBER_OF_CELLS/NUMBER_OF_ROWS;
+
+        
+
+        let content = rows.map((rows) =>
+            {
+                <div className='row'>
+                       
+                </div>
+            }
+        );
+
+        
+
         return(
             <div className="image-grid">
-                {content}
             </div>
         );
     }
